160ef25b81407889017cef98b3ad32e9845f6c7f
jessicatrejo32 <142848231+jessicatrejo32@users.noreply.github.com>
2024-09-03 15:42:13 -0600

    Update pytestpublicsv.py

--- pytestpublicsv.py
+++ pytestpublicsv.py
@@ -136,8 +136,8 @@ def test_validacion_datos(setup, df, allure_story, valor, selector, ruta, screen
     # Establecer un título dinámico para la prueba
     allure.dynamic.title(allure_story)
 
-    #valor_csv = "{:,.0f}".format(int(df[valor].iloc[0]))
-    valor_csv = df['PORCENTAJE_ACTAS_CONTABILIZADAS'].str.replace('%', '', regex=False)
+    valor_csv = "{:,.0f}".format(int(df[valor].iloc[0]))
+    #valor_csv = df['PORCENTAJE_ACTAS_CONTABILIZADAS'].str.replace('%', '', regex=False)
     # Convertir el tipo de localizador a su objeto correspondiente de Selenium
     locator_type_obj = eval(selector)
     
