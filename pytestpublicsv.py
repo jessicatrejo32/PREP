d8b2120bc92df79702481d01905c6a641fe01255
jessicatrejo32 <142848231+jessicatrejo32@users.noreply.github.com>
2024-09-03 15:32:44 -0600

    Update pytestpublicsv.py

--- pytestpublicsv.py
+++ pytestpublicsv.py
@@ -136,7 +136,7 @@ def test_validacion_datos(setup, df, allure_story, valor, selector, ruta, screen
     # Establecer un título dinámico para la prueba
     allure.dynamic.title(allure_story)
 
-    valor_csv = "{:,.0f}".format(int(df[valor].iloc[0]))
+    #valor_csv = "{:,.0f}".format(int(df[valor].iloc[0]))
     valor_csv = df['PORCENTAJE_ACTAS_CONTABILIZADAS'].astype(str)
     valor_csv = df['PORCENTAJE_ACTAS_CONTABILIZADAS'].str.replace('%', '', regex=False)
     valor_csv = df['PORCENTAJE_ACTAS_CONTABILIZADAS'].astype(float) / 100
