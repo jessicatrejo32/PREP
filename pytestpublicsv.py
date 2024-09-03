1da6bb6bd98df455f10edddb8b94806f6e1459e8
jessicatrejo32 <142848231+jessicatrejo32@users.noreply.github.com>
2024-09-03 13:43:07 -0600

    Update pytestpublicsv.py

--- pytestpublicsv.py
+++ pytestpublicsv.py
@@ -135,7 +135,7 @@ def test_validacion_datos(setup, df, allure_story, valor, selector, ruta, screen
     # Establecer un título dinámico para la prueba
     allure.dynamic.title(allure_story)
 
-    valor_csv = "{:,.0f}".format(string(df[valor].iloc[0]))
+    valor_csv = "{:,.0f}".format(int(df[valor].iloc[0]))
 
   
 
